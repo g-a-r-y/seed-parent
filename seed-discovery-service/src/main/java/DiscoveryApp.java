33bf99eb3c2fc7806f307cfad40e6f181382fb16
gary <gary@hudongpai.com>
2016-11-30 10:28:43 +0800

    init project

---
+++ seed-discovery-service/src/main/java/DiscoveryApp.java
@@ -0,0 +1,16 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * Created by gary on 16/11/29.
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class DiscoveryApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiscoveryApp.class, args);
+	}
+}
