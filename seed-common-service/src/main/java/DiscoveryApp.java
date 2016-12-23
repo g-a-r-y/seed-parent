fc80ada303b1d0f8695e5b88a3dee4257ea6e042
gary <gary@hudongpai.com>
2016-12-23 14:19:02 +0800

    fix project

--- seed-common-service/src/main/java/DiscoveryApp.java
+++ seed-common-service/src/main/java/DiscoveryApp.java
@@ -1,5 +1,6 @@
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 import java.util.Objects;
