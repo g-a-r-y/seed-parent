c897e1fa616f8c566420fa736e8d17b355d3bc20
gary <gary@hudongpai.com>
2017-03-03 18:48:24 +0800

    pom update

---
+++ seed-user-service/src/main/java/com/gary/user/UserApp.java
@@ -0,0 +1,16 @@
+package com.gary.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by gary on 17/2/23.
+ */
+
+@SpringBootApplication
+public class UserApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserApp.class, args);
+	}
+}
